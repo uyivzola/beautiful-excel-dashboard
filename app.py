25f2172ed3914707e1a4e5a0144543f7e0792793
hulacorn <61078606+uyivzola@users.noreply.github.com>
2023-04-21 22:33:41 +0000

    importing essential libraries

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
