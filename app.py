bfde205aefc28ad65620932f1a18f4d6e44fb9ea
hulacorn <61078606+uyivzola@users.noreply.github.com>
2023-04-21 22:43:24 +0000

    not star but lemon

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown("##")
 # TOP KPI's
 total_sales = int(df_selection["Total"].sum())
 average_rating = round(df_selection["Rating"].mean(), 1)
-star_rating = ":star:" * int(round(average_rating, 0))
+star_rating = ":lemon:" * int(round(average_rating, 0))
 average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
 
 left_column, middle_column, right_column = st.columns(3)
