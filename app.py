09c8d0cfa7c7be38b7942530ca6ab18f1126cb8d
hulacorn <61078606+uyivzola@users.noreply.github.com>
2023-04-21 22:37:11 +0000

    Read excel file and cache

--- app.py
+++ app.py
@@ -3,3 +3,20 @@ import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+# ---- READ EXCEL ----
+@st.cache
+def get_data_from_excel():
+    df = pd.read_excel(
+        io="supermarkt_sales.xlsx",
+        engine="openpyxl",
+        sheet_name="Sales",
+        skiprows=3,
+        usecols="B:R",
+        nrows=1000,
+    )
+    # Add 'hour' column to dataframe
+    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    return df
+
+df=get_data_from_excel()
