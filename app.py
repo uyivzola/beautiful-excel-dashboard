024b0ec864b989f347d91edeeb02e1ceaf0543a7
hulacorn <61078606+uyivzola@users.noreply.github.com>
2023-04-21 22:48:03 +0000

    sales by product line - bar chart

--- app.py
+++ app.py
@@ -68,3 +68,21 @@ with right_column:
     st.subheader(f"US $ {average_sale_by_transaction}")
 
 st.markdown("""---""")
+
+# SALES BY PRODUCT LINE [BAR CHART]
+sales_by_product_line = (
+    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+)
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x="Total",
+    y=sales_by_product_line.index,
+    orientation="h",
+    title="<b>Sales by Product Line</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+    template="plotly_white",
+)
+fig_product_sales.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
